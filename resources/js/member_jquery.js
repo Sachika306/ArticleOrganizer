0e52605344e740637b5de4040d61dea28d1398c0
Sachika Baba <sachikababa306@gmail.com>
2021-03-29 20:07:38 +0900

    [update]refs #9　オートコンプリートの項目選択時にすべてのオプションを表示させる

--- resources/js/member_jquery.js
+++ resources/js/member_jquery.js
@@ -13,25 +13,30 @@ $('.date').datepicker({
 
 
 // オートコンプリート機能を適用（article.assign）
+$( "#outline_user_name" ).on( "autocompletefocus", function( event, ui ) {} );
+
 $( "#outline_user_name" ).autocomplete({
   source: outlineUserNames,
+  minLength: 0,
   select: function(event, ui) {
     $("#outline_user_name").val(ui.item.label);
     $("#outline_user_id").val(ui.item.value);
     return false;
-    //event.preventDefault(); // inputにvalueが流れる（デフォルトの設定）を防ぐ
-  },
-  autofocus: true
+  }
+}).focus(function(){
+  $(this).autocomplete('search', $(this).val());
 });
 
 console.log(outlineUserNames);
 
 $( "#article_user_name" ).autocomplete({
   source: articleUserNames,
+  minLength: 0,
   select: function(event, ui) {
     $("#article_user_name").val(ui.item.label);
-    event.preventDefault(); // inputにvalueが流れる（デフォルトの設定）を防ぐ
     $("#article_user_id").val(ui.item.value);
-  },
-  autofocus: true
+    return false;
+  }
+}).focus(function(){
+  $(this).autocomplete('search', $(this).val());
 });
