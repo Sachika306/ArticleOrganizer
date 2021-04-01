ebfa6198a6e67edf457efc55660e706c0432e8e9
Sachika Baba <sachikababa306@gmail.com>
2021-04-02 00:16:13 +0900

    [update]refs #17 フッターの追加

--- resources/views/components/aa.blade.php
+++
@@ -1,4 +0,0 @@
-<form action="/article/destroy/{{ $article->id }}" id="delete" method="post">
-                  @csrf
-                  <button type="submit" id="delete">削除</button>
-                  </form>
