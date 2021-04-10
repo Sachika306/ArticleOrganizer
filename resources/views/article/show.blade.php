c10a9086ed895de9e0ef04834a8c4dfa06076605
Sachika Baba <sachikababa306@gmail.com>
2021-04-11 01:44:10 +0900

    [add]refs #29 Edited to show S3's images

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -90,7 +90,7 @@
             <h3 class="display-6">画像</h3>
             @isset($article->thumbnail->file_name)
             <div class="form-group">
-              <img style="max-height: 500px; max-width: 100%; object-fit: cover; object-position: center; max-width: 100%; border-radius: 0.25rem; border: 1px solid #ced4da;" src="{{ asset('/storage/thumbnails/'.$article->thumbnail->file_name) }}"> 
+              <img style="max-height: 500px; max-width: 100%; object-fit: cover; object-position: center; max-width: 100%; border-radius: 0.25rem; border: 1px solid #ced4da;" src="{{ Storage::url($article->thumbnail->file_name) }}"> 
             </div>
             @else 
             <div class="form-group" style="height: 300px; width: 100%; object-fit: cover; object-position: left top; max-width: 100%; border-radius: 0.25rem; border: 1px solid #ced4da; background-color: gray;">
