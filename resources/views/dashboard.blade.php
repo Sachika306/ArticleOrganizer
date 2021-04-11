ec2b2e8f1b2f8d9a2c5a4ec762223eab1d994cea
Sachika Baba <sachikababa306@gmail.com>
2021-04-11 13:11:25 +0900

    [update]refs #29 S3で保存した画像が表示されるように編集

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,25 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
                 <div class="card-body">
                     <p>こんにちは、{{ Auth::user()->name }}さん</p>
-           
+                    <ul>
+                        <li>
+                            <a href="{{ url('/') }}" target="_blank">公開中サイトを確認（別タブで開きます）</a>
+                        </li>
+                        @can('admin-user')
+                        <li>
+                            <a href="{{ url('/member') }}">メンバー一覧</a>
+                        </li>
+                        @endcan
+                        <li>
+                            <a href="{{ url('/article') }}">記事一覧</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/member/setting') }}">設定変更</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/logout') }}">ログアウト</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
