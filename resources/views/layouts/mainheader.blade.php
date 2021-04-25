04ae6704740f9e662c8e4bb99f91c17efa6b0808
Sachika306 <sachikababa306@gmail.com>
2021-04-26 00:48:03 +0900

    added s to the url

--- resources/views/layouts/mainheader.blade.php
+++ resources/views/layouts/mainheader.blade.php
@@ -17,7 +17,7 @@
       <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     @endif
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
 </head>
 <body>
